ba87f512414509116aa17ab1dfedb49efc74b7a4
fanglm <ooohmydawn@hotmail.com>
2023-03-24 15:33:33 +0800

    feat: add guide page

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,61 +1,16 @@
 // pages/home/index.js
+import {
+	reLaunch,
+	setItemSync
+} from '~/utils/util';
+import {
+	Chat
+} from '~/utils/router';
 Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {
-   
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad() {
+	toChat() {
+		setItemSync('senior', true)
+		reLaunch({
+			url: Chat.path,
+		})
 	},
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {},
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {},
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {},
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {},
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {},
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {},
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-    const imageUrl = shareImageFormat(shareImage);
-    return {
-      title: '分享小程序标题',
-      imageUrl,
-    };
-  },
-
-  navigateTo(e) {
-    const { path } = e.currentTarget.dataset;
-    navigateTo({ url: path });
-  },
 });
